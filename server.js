82abad7843b8792439b4639c997d22dd0e3096fe
LucieO <luciecamilleory@gmail.com>
2020-04-06 19:13:39 +0200

    heroku test

--- server.js
+++ server.js
@@ -19,11 +19,18 @@ app.use(json({ limit: '2mb' }))
 const MONGO_URI = `mongodb+srv://${process.env.MONGO_ADMIN}:${process.env.MONGO_PASSWORD}@cluster0-jjo7q.mongodb.net/${process.env.MONGO_DB}`;
 console.log(MONGO_URI)
 console.log(process.env.front_url)
-const corsOptions = {
-    origin: process.env.front_url,
-    credentials: true,
-  };
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: process.env.front_url,
+//     credentials: true,
+//   };
+// app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 const getUser = async (token) => {
   try {
